d878cfe1c54b8d85b8f63171fa8aeb76be42468e
yihimin <yihimin01@gmail.com>
2025-04-01 16:54:58 +0900

    feat: posts mock 데이터 생성

--- src/mocks/posts.ts
+++ src/mocks/posts.ts
@@ -1,4 +1,24 @@
-import { Post } from "@/types/Post";
+export interface Author {
+  id: number;
+  username: string;
+  role: string;
+}
+
+export interface Post {
+  id: number;
+  title: string;
+  content: string;
+  author: Author;
+  category: "공지" | "자유게시판" | "일정" | "통계";
+  createdAt: string;
+  updatedAt: string;
+  location?: string;
+  time?: string;
+  tags: string[];
+  likes: number;
+  comments: number;
+  saved: boolean;
+}
 
 export const mockPosts: Post[] = [
   {
