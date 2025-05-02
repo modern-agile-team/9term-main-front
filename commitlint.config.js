c37779f94e686c08d4c4e3f6928656252d5b4299
yihimin <yihimin01@gmail.com>
2025-05-02 18:07:24 +0900

    setting: commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,26 @@
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "subject-case": [0], // 대소문자 검사 끔
-    "type-empty": [0], // type 비워도 OK
-    "subject-empty": [0], // subject 비워도 OK
+    'subject-case': [0], // 대소문자 검사 끔
+    'type-empty': [0], // type 비워도 OK
+    'subject-empty': [0], // subject 비워도 OK
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'setting', // 추가적으로 허용할 커밋 유형
+      ],
+    ],
   },
 };
